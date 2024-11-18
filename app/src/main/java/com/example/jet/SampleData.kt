24cf406206aa24afef872c772fda1c01d726e2cc
cordex5 <cordex135@gmail.com>
2024-11-18 22:17:10 +0100

    initial Commit

---
+++ app/src/main/java/com/example/jet/SampleData.kt
@@ -0,0 +1,59 @@
+import com.example.jet.Message
+
+object SampleData {
+    // Przykładowe dane konwersacji
+    val conversationSample = listOf(
+        Message(
+            "Maciek",
+            "Dlaczego programista chodzi na siłownię? Żeby mieć mocne funkcje!"
+        ),
+        Message(
+            "Maciek",
+            "Jakie są trzy ulubione rzeczy programisty? Debugowanie, przerwa na kawę i... debugowanie przerwy na kawę."
+        ),
+        Message(
+            "Maciek",
+            """Jak wygląda ulubiona przekąska programisty?
+            |- Ciastka... ale tylko w plikach cookie!""".trim()
+        ),
+        Message(
+            "Maciek",
+            "Hej! Wiesz, dlaczego Android nie zaprasza Windowsa na imprezy? Bo zawsze się zawiesza!"
+        ),
+        Message(
+            "Maciek",
+            """Czy słyszałeś o tym programiście, który napisał poemat w Kotlinie?
+            |Był tak dobrze zoptymalizowany, że każde słowo miało tylko jedno wywołanie funkcji!""".trim()
+        ),
+        Message(
+            "Maciek",
+            "Czy wiesz, dlaczego programiści nie lubią wychodzić na dwór? Bo zawsze wolą \"wewnętrzne\" interfejsy!"
+        ),
+        Message(
+            "Maciek",
+            "Jak wygląda ulubiony dzień programisty? Debugowanie w piżamie z kawą w ręku!"
+        ),
+        Message(
+            "Maciek",
+            """Jak programista flirtuje?
+            |\"Chciałbym dodać cię do mojej listy obiektów, bo jesteś wyjątkowa!\"""".trim()
+        ),
+        Message(
+            "Maciek",
+            "Czy widziałeś najnowszy feature Androida? Możesz teraz debugować swoją poranną kawę!"
+        ),
+        Message(
+            "Maciek",
+            """Czemu programista nie boi się hakerów?
+            |Bo jego hasła to linia kodu, której sam nie rozumie!""".trim()
+        ),
+        Message(
+            "Maciek",
+            "Czy wiesz, co programista robi na wakacjach? Przegląda GitHub pod palmami!"
+        ),
+        Message(
+            "Maciek",
+            "Właśnie odkryłem, że mogę skompilować swój humor! Szkoda, że wyszły tylko błędy!"
+        )
+    )
+}
